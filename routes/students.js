9ab02d00371e6c3c34ff9d12af32bed8c3fbacb8
Reyaad Rafeek <reyaad510@gmail.com>
2019-05-28 22:25:18 -0400

    Created seeds 001-cohorts

--- routes/students.js
+++ routes/students.js
@@ -11,8 +11,13 @@ const knexConfig = {
 
 const db = knex(knexConfig);
 
-router.get('/', (req, res) => {
-    
+router.get('/', async (req,res) => {
+    try {
+        const students = await db('students');
+        res.status(200).json(students);
+    } catch(err) {
+        res.status(500).json(err);
+    } 
 })
 
 
