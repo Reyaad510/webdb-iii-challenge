ee6587f1cd5ae059ec1560917893997c3f22ab6c
Reyaad Rafeek <reyaad510@gmail.com>
2019-05-28 22:53:15 -0400

    Students showing on correct route with seeds. MVP completed

--- seeds/002-students.js
+++ seeds/002-students.js
@@ -6,9 +6,9 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('students').insert([
-        {name: 'Reyaad Student', cohort_id: 1},
-        {name: 'Aleem Student', cohort_id: 1},
-        {name: 'Adam Student', cohort_id: 1}
+        {name: 'Reyaad Student', cohort_id: '1'},
+        {name: 'Aleem Student', cohort_id: '1'},
+        {name: 'Adam Student', cohort_id: '1'}
       ]);
     });
 };
