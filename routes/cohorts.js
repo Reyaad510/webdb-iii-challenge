50ea909081bfd1823f0db333113edf85c874cef5
Reyaad Rafeek <reyaad510@gmail.com>
2019-05-28 21:51:24 -0400

    Scaffold routes(cohorts and students)

---
+++ routes/cohorts.js
@@ -0,0 +1,11 @@
+const knex = require('knex');
+const router = require('express').Router();
+
+
+router.get('/', (req,res) => {
+   
+})
+
+
+module.exports = router;
+
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const helmet = require('helmet');
 
+const cohortsRouter = require('./routes/cohorts.js');
+const studentsRouter = require('./routes/students');
+
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
+
+server.use('/api/cohorts', cohortsRouter);
+server.use('/api/students', studentsRouter);
 
 module.exports = server;
