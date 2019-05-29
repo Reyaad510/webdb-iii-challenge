4ced845150891ff4da6aa0f728077874621dd4a9
Reyaad Rafeek <reyaad510@gmail.com>
2019-05-28 22:03:39 -0400

    created lambda.db3, added knexfile

--- routes/cohorts.js
+++ routes/cohorts.js
@@ -2,8 +2,23 @@ const knex = require('knex');
 const router = require('express').Router();
 
 
-router.get('/', (req,res) => {
-   
+const knexConfig = {
+    client: 'sqlite3',
+    connection: {
+        filename: './data/lambda.db3'
+    },
+    useNullAsDefault: true,
+}
+
+const db = knex(knexConfig);
+
+router.get('/', async (req,res) => {
+    try {
+        const cohorts = await db('cohorts');
+        res.status(200).json(cohorts);
+    } catch(err) {
+        res.status(500).json(err);
+    } 
 })
 
 
